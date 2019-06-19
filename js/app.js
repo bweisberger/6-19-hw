5456bda09c714d71914f68778a1a80144c9bfa86
William Weisberger <bill.weisberger@gmail.com>
2019-06-19 15:37:31 -0600

    Commit 3 - Excited Kittens answered

--- js/app.js
+++ js/app.js
@@ -5,3 +5,14 @@ for (let i = 1; i <= 20; i++) {
 for (let i = 0; i <= 200; i += 2) {
   console.log(i);
 }
+
+
+const catTalk = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."]
+for (let i = 0; i <= 20; i++) {
+  if (i % 2 !== 0) {
+    //log a variable in catTalk using Math.floor(Math.random()*3)
+    console.log(catTalk[Math.floor(Math.random()*3)]);
+  } else {
+    console.log("Love me, pet me! HSSSSS!");
+  }
+}
