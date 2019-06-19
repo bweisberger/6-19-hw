d3d32f63ea9f5bb588b090c48bbf728153298354
William Weisberger <bill.weisberger@gmail.com>
2019-06-19 15:42:44 -0600

    Commit 4 - FizzBuzz answered

--- js/app.js
+++ js/app.js
@@ -16,3 +16,18 @@ for (let i = 0; i <= 20; i++) {
     console.log("Love me, pet me! HSSSSS!");
   }
 }
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  }
+  else if (i % 3 === 0) {
+    console.log("Fizz");
+  }
+  else if (i % 5 === 0) {
+    console.log("Buzz") ;
+  }
+  else {
+    console.log(i);
+  }
+}
